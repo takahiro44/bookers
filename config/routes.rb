72631b3f85baf7f143958ec4acf1ad39364f1023
EC2 Default User <ec2-user@ip-172-31-47-225.ap-northeast-1.compute.internal>
2025-05-07 07:21:54 +0000

    [Add]ルートの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'bookers' => 'homes#top'
+  get 'bookers/books' => 'books#index'
+  get 'bookers/books/:id' => 'books#show'
+  get 'bookers/books/:id/edit' => 'books#edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
