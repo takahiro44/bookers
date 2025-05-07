dd8f8dd38c315e960e69c2850b7ad533fc4da0c9
EC2 Default User <ec2-user@ip-172-31-47-225.ap-northeast-1.compute.internal>
2025-05-07 11:27:02 +0000

    [Add]諸機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'bookers' => 'homes#top'
-  get 'bookers/books' => 'books#index'
-  get 'bookers/books/:id' => 'books#show'
-  get 'bookers/books/:id/edit' => 'books#edit'
+  root 'homes#top'
+  resources :books
+  #get 'bookers/books' => 'books#index'
+  #post 'bookers/books' => 'books#create'
+  #get 'bookers/books/:id' => 'books#show'
+  #get 'bookers/books/:id/edit' => 'books#edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
