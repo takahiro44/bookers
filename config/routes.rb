bc391bcaa4eb4769926c3c364462348b9869affc
EC2 Default User <ec2-user@ip-172-31-47-225.ap-northeast-1.compute.internal>
2025-05-09 08:41:51 +0000

    [Add] レイアウト,フラッシュメッセージ

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#top'
   resources :books
-  #get 'bookers/books' => 'books#index'
-  #post 'bookers/books' => 'books#create'
-  #get 'bookers/books/:id' => 'books#show'
-  #get 'bookers/books/:id/edit' => 'books#edit'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
